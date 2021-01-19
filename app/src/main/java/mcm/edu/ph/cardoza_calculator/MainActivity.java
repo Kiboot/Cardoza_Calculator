bc06e9b9917af80407347c675505a8898e6ebc7a
Prg <kevinlee.cardoza@gmail.com>
2021-01-19 10:51:04 +0800

    removed gachi
    refactor button names

--- app/src/main/java/mcm/edu/ph/cardoza_calculator/MainActivity.java
+++ app/src/main/java/mcm/edu/ph/cardoza_calculator/MainActivity.java
@@ -3,6 +3,7 @@ package mcm.edu.ph.cardoza_calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    @Override
+    public void onClick(View view){
+
+    }
+
 }
